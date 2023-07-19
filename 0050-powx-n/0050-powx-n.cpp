0f8786d9fd1b541e9b4c140f8861e2436e9c27f4
Yash Sinha <sinhayash2001@gmail.com>
2023-07-19 11:29:18 +0530

    Time: 4 ms (40.22%), Space: 5.8 MB (92.92%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n < 0){
+            x = 1/x;
+            n =abs(n);
+        }
+        if (n == 0) return 1;
+        if (n == 2) return x*x;
+        if (n % 2 == 0) return myPow(myPow(x,n/2), 2);
+        else return x* myPow(myPow(x,n/2), 2);
+    }
+};
